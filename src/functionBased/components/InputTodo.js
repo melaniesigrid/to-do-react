a3ec499202c9d637cef34d3d7e28f14f5efaf4b9
Melanie Arellano <melaniesigrid@protonmail.com>
2022-02-15 23:09:04 -0600

    Create React Router, used version 5.2.0 for compatibility with tutorial

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,40 +1,45 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
+import { FaPlusCircle } from "react-icons/fa"
 
-class InputTodo extends Component {
-  state = {
-    title: ""
-  };
-  onChange = e => {
-    this.setState({
+const InputTodo = props => {
+  const [inputText, setInputText] = useState({
+    title: "",
+  })
+
+  const onChange = e => {
+    setInputText({
+      ...inputText,
       [e.target.name]: e.target.value,
     })
-  };
-  handleSubmit = e => {
+  }
+
+  const handleSubmit = e => {
     e.preventDefault()
-    if (this.state.title.trim()) {
-      this.props.addTodoProps(this.state.title)
-      this.setState({
+    if (inputText.title.trim()) {
+      props.addTodoProps(inputText.title)
+      setInputText({
         title: "",
       })
     } else {
       alert("Please write item")
     }
   }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Add Todo..."
-          value={this.state.title}
-          name="title"
-          onChange={this.onChange}
-        />
-        <button className="input-submit">Submit</button>
-      </form>
-    )
-  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={inputText.title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">
+        <FaPlusCircle style={{ color: "darkcyan", fontSize: "20px", marginTop: "2px" }} />
+      </button>
+    </form>
+  )
 }
 
-export default InputTodo;
+export default InputTodo
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
 //component file
 import TodoContainer from "./functionBased/components/TodoContainer";
 import "./functionBased/App.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
-  </React.StrictMode>, document.getElementById("root"));
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
